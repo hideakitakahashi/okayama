a7765174a44c6528dc8ef58d949d34587dc3b12e
hideakitakahashi <info@shizencyokuhan.org>
2016-12-26 14:19:58 +0900

    design 2  Please enter the commit message for your changes. Lines starting

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   has_many :comments
   default_scope -> { order(created_at: :desc) }
   validates :member_id, presence: true
-  CATEGORY_NAMES = %w( 農産畜産品 加工食品(味噌、海産物など) パン、お菓子 日用雑貨 売店食堂 工房、アート、おもちゃ 遊ぶ イベント紹介 メンタルケア 移住、田畑の相談 グループ紹介 アースシップを造りたい あげます欲しいですコーナー )
+  CATEGORY_NAMES = %w( 農産畜産品 加工食品(味噌、海産物など) パン、お菓子 売店食堂 雑貨、工房、アート、おもちゃ イベント紹介 メンタルケア 移住、田畑の相談 グループ紹介 その他・雑談 あげます欲しいですコーナー )
 end
